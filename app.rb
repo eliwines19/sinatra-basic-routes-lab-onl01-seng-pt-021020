58374fa7532425967083c7699f972e4e7f5c7482
Elijah wines <elijahwines84@yahoo.com>
2020-05-01 16:54:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
+  get '/' do
     puts "Hello, World!"
   end
 
@@ -10,11 +10,11 @@ class App < Sinatra::Base
     puts "My name is __"
   end
 
-  get '/hometown' do 
+  get '/hometown' do
     puts "My hometown is __"
   end
 
-  get '/favorite-song' do 
+  get '/favorite-song' do
     puts "My favorite song is __"
   end
 
