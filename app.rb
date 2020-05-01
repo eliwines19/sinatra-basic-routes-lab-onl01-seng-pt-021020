fd5e184a4b00420dd16835a4def6a3b1b08cb7cb
Elijah wines <elijahwines84@yahoo.com>
2020-05-01 16:55:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,19 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    puts "Hello, World!"
-  end
-
+      "Hello, World!"
+  end 
   get '/name' do
-    puts "My name is __"
-  end
-
+      "My name is __"
+  end 
   get '/hometown' do
-    puts "My hometown is __"
-  end
-
+      "My hometown is __"
+  end 
   get '/favorite-song' do
-    puts "My favorite song is __"
-  end
+      "My favorite song is __" 
+  end 
 
 end
